d574d7a571ff866874be4c9711bd3db271b855ae
omniarchy <k.hayashi@cresplanex.com>
2024-12-24 05:28:42 +0900

    fix

--- src/main/java/org/cresplanex/nova/job/exception/GrpcExceptionAdvice.java
+++ src/main/java/org/cresplanex/nova/job/exception/GrpcExceptionAdvice.java
@@ -5,19 +5,25 @@ import build.buf.gen.job.v1.JobServiceErrorMeta;
 import build.buf.gen.job.v1.JobServiceInternalError;
 import build.buf.gen.userprofile.v1.*;
 import io.grpc.Status;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 
+@Slf4j
 @GrpcAdvice
 public class GrpcExceptionAdvice {
 
     @GrpcExceptionHandler
     public Status handleInternal(Throwable e) {
+        log.error("Internal error", e);
+
+        String message = e.getMessage() != null ? e.getMessage() : "Unknown error occurred";
+
          JobServiceInternalError.Builder descriptionBuilder =
                  JobServiceInternalError.newBuilder()
                          .setMeta(JobServiceErrorMeta.newBuilder()
                                  .setCode(JobServiceErrorCode.JOB_SERVICE_ERROR_CODE_INTERNAL)
-                                 .setMessage(e.getMessage())
+                                 .setMessage(message)
                                  .build());
 
          return Status.INTERNAL
